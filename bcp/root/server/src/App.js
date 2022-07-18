c9467608974478dddb7d778c17fa50b019f16776
Rae Long <rae@stocksoftware.com.au>
2022-07-18 19:14:10 +1000

    create asset mode filter

--- bcp/root/server/src/App.js
+++ bcp/root/server/src/App.js
@@ -1,25 +1,31 @@
 import React, {useState} from 'react';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import {Dropdown} from 'react-bootstrap';
 import Map from './pages/map';
 import List from './pages/list';
 import './less/app.less';
-// import 'react-bootstrap/dist/'
+import {ASSET_MODE} from "./constant";
 const App = () => {
     const [key, setKey] = useState('map');
+    const [asset, setAsset] = useState(ASSET_MODE.aircraft);
 
     return (
         <div id="app">
             <div className="navFilter">
-            <div>
-                Mode
-            </div>
+                <Dropdown id="modeFilter">
+                    <Dropdown.Toggle  id="dropdown-mode">
+                       MODE:{asset}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={()=>{setAsset(ASSET_MODE.aircraft);}}>{ASSET_MODE.aircraft}</Dropdown.Item>
+                        <Dropdown.Item onClick={()=>{setAsset(ASSET_MODE.equipment);}}>{ASSET_MODE.equipment}</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
              <ul id="viewFilter">
-                 <li className={key==='map'? 'active btn':'btn'} onClick={()=>{setKey('map')}}>MAP</li>
-                 <li className={key==='list'?'active btn':'btn'} onClick={()=>{setKey('list')}}>LIST</li>
+                 <li className={key==='map'? 'active btn':'btn'} onClick={()=>{setKey('map');}}>MAP</li>
+                 <li className={key==='list'?'active btn':'btn'} onClick={()=>{setKey('list');}}>LIST</li>
              </ul>
             </div>
-            {key==='map'? <Map/> : <List/>}
+            {key==='map'? <Map asset={asset}/> : <List/>}
         </div>
     );
 };
---
+++ bcp/root/server/src/constant/index.js
@@ -0,0 +1,4 @@
+export const ASSET_MODE={
+    aircraft: 'AIRCRAFT',
+    equipment: 'EQUIPMENT'
+};
