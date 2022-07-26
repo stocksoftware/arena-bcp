3209e53feb4c72b578ed0180025a55da3346ecda
Rae Long <rae@stocksoftware.com.au>
2022-07-26 10:33:00 +1000

    create project structure

--- bcp/root/server/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import * as ReactDOM from "react-dom/client";
-import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
---
+++ bcp/web/src/webpack/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+
+const App = () => {
+
+    return (
+        <div id="app">
+         app
+        </div>
+    );
+};
+
+export default App;
