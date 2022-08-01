398da952b8ebf732a13540df4dd29f9b7d7a4606
Rae Long <rae@stocksoftware.com.au>
2022-08-01 12:01:55 +1000

    update backup shell file

--- backup.sh
+++ backup.sh
@@ -5,3 +5,9 @@ mkdir -p .data
 node dist/scripts/backup.js
 cp .data/*.json dist/client/data
 cp .data/*.csv dist/client/reports
+cp .data/*.json bcp/public/data
+cp .data/*.csv bcp/public/reports
+mkdir -p bcp/public/data
+mkdir -p bcp/public/reports
+cp .data/*.json bcp/public/data
+cp .data/*.csv bcp/public/reports
