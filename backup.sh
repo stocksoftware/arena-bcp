5669cc716fb166e7ae6bebec4c16362f70db6711
Rae Long <rae@stocksoftware.com.au>
2022-08-01 14:47:08 +1000

    styles

--- backup.sh
+++ backup.sh
@@ -5,9 +5,8 @@ mkdir -p .data
 node dist/scripts/backup.js
 cp .data/*.json dist/client/data
 cp .data/*.csv dist/client/reports
-cp .data/*.json bcp/public/data
-cp .data/*.csv bcp/public/reports
 mkdir -p bcp/public/data
 mkdir -p bcp/public/reports
 cp .data/*.json bcp/public/data
 cp .data/*.csv bcp/public/reports
+
--- bcp/client/src/webpack/helper/map-display.js
+++ bcp/client/src/webpack/helper/map-display.js
@@ -257,3 +257,4 @@ export function getAircraftSilhouettePath(aircraft) {
     }
     return silhouettePath;
 }
+
