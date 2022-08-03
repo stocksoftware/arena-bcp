45765aacad0849d6e49f5eb7674460a9978f79a7
Rae Long <rae@stocksoftware.com.au>
2022-08-03 17:03:22 +1000

    update availability popup

--- bcp/client/src/webpack/pages/List.js
+++ bcp/client/src/webpack/pages/List.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import useStores from "../hooks/use-stores";
 import {observer} from "mobx-react";
+
 const List = observer(() => {
     const {mapStore} = useStores();
     const assetMode = mapStore.assetType;
 
     return (
         <div>
-            list
+            List
         </div>
     );
 });
