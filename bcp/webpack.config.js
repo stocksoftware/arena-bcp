aa7212f0b7f3088ee3c4f723631cb5e8485f2e0a
Rae Long <rae@stocksoftware.com.au>
2022-08-08 13:20:59 +1000

    fix: nested rules

--- bcp/webpack.config.js
+++ bcp/webpack.config.js
@@ -61,9 +61,12 @@ const clientConfig = {
                         resourceQuery: /component/,
                         use: [
                             {
-                                loader:'react-svg-loader',
+                                loader: 'babel-loader'
+                            },
+                            {
+                                loader: 'react-svg-loader',
                                 options: {
-                                            jsx: true // true outputs JSX tags
+                                    jsx: true // true outputs JSX tags
                                 }
                             }
                         ]
